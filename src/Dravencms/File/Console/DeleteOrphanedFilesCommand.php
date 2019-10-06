436ffb939bb59ceafdf847ff6a2a8c081a575754
Adam Schubert <adam.schubert@sg1-game.net>
2019-10-06 06:30:34 +0200

    Update DeleteOrphanedFilesCommand.php

--- src/Dravencms/File/Console/DeleteOrphanedFilesCommand.php
+++ src/Dravencms/File/Console/DeleteOrphanedFilesCommand.php
@@ -9,6 +9,7 @@ use Salamek\Files\FileStorage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
